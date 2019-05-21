f09667ec1984d4fc3d9217aa2c344a79693b8ff6
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-21 01:43:31 -0300

    Adding navigation to project

---
+++ src/Menu.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { createDrawerNavigator } from "react-navigation"
+import { Text, StyleSheet } from "react-native"
+
+import Simples from "./components/Simples"
+import Parimpar from "./components/Parimpar"
+import { Inverter, MegaSena } from "./components/Multi"
+
+export default createDrawerNavigator({
+  MegaSena: {
+    screen: () => <MegaSena numeros={8}/>,
+    navigationOptions: { title: "Mega Sena" }
+  },
+  Inverter: {
+    screen: () => <Inverter texto="React Nativo!"/>
+  },
+  Parimpar: {
+    screen: () => <Parimpar numero={33}/>,
+    navigationOptions: { title: "Par/Ímpar" }
+  },
+  Simples: {
+    screen: () => <Simples texto="Flexível!"/>
+  },
+  Texto: {
+    screen: () => <Text style={styles.f20}>App!</Text>
+  }
+}, { drawerWidth: 300 })
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  f20: {
+    fontSize: 20
+  }
+})
