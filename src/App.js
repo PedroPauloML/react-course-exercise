0ef12122607a3a80484ee880ef7dd6cc9ed0090c
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-21 01:09:16 -0300

    Conditional renderization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
 import { View, Text, StyleSheet } from "react-native"
+
 import Simples from "./components/Simples"
+import Parimpar from "./components/Parimpar"
+import If from "./components/If"
 
 export default class App extends Component {
   render() {
@@ -8,6 +11,7 @@ export default class App extends Component {
       <View style={styles.container}>
         <Text style={styles.f20}>App!</Text>
         <Simples texto="Flexível!"/>
+        <Parimpar numero={33}/>
       </View>
     )
   }
--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,4 +1,6 @@
 import React from "react"
 import { Text } from "react-native"
+import Padrao from "../estilo/Padrao"
 
-export default (props) => <Text>{props.texto}</Text>
+export default (props) =>
+  <Text style={Padrao.ex}>{props.texto}</Text>
