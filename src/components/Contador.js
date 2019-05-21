56645f74a362ffdee1d1c52f1f85a1951a501345
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2019-05-21 02:18:38 -0300

    Caution with 'this'

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -7,19 +7,26 @@ export default class Contador extends Component {
     numero: this.props.numero || 0
   }
 
-  maisUm = () => {
+  // constructor(props) {
+  //   super(props)
+
+  //   this.maisUm = this.maisUm.bind(this)
+  //   this.limpar = this.limpar.bind(this)
+  // }
+
+  maisUm() {
     this.setState({ numero: this.state.numero + 1 })
   }
   
-  limpar = () => { this.setState({ numero: this.props.numero || 0 }) }
+  limpar() { this.setState({ numero: this.props.numero || 0 }) }
 
   render() {
     return (
       <View>
         <Text style={{fontSize: 30}}>{this.state.numero}</Text>
         <TouchableHighlight
-          onPress={this.maisUm}
-          onLongPress={this.limpar}>
+          onPress={() => this.maisUm()}
+          onLongPress={() => this.limpar()}>
           <Text style={Padrao.btn}>Incrementar/Zerar</Text>
         </TouchableHighlight>
       </View>
